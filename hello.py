54b88334eaa2c698d12725b4501eb1e68c37b58b
YOUR NAME <YOUR@EMAIL.com>
2020-01-07 08:19:53 +0545

    small change

--- hello.py
+++ hello.py
@@ -4,5 +4,9 @@ app = Flask(__name__)
 def hello():
     return "hello world"
 
+@app.route("/ping")
+def ping():
+    return "pong"
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
